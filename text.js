eb46ecad20829430c5119b60e8762eb6f8353257
Rozietka <morkvenas.vitalijus@gmail.com>
2022-03-15 20:51:26 +0200

    text done

--- text.js
+++ text.js
@@ -1,7 +1,11 @@
-const text = {};
+const text = {
+    first: s => s[0], // s string'as
+    middle: s => s[Math.floor((s.length - 1) / 2)], // s string'as
+    last: s => s[s.length -1], // s string'as 
+};
 
-const textAction = 'first'; // first | middle | last
-const word = 'Labas';
-const letter = 'x';
+const textAction = 'middle'; // first | middle | last
+const word = 'abdc';
+const letter = text[textAction](word);
 
 console.log(`${textAction} letter of word "${word}" is ${letter}`);
