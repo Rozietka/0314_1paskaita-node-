3e55f36bb4e4391a625cdc8270104f7d16a447e3
Rozietka <morkvenas.vitalijus@gmail.com>
2022-03-15 19:50:07 +0200

    pirmo_done_naujas_math

--- index.js
+++ index.js
@@ -28,17 +28,30 @@ const people = [
 ]
 console.log(people);
 
-const colors = [];
+// atsirenkame kokiu spalvu ir kokiais kiekiais yra masyve
+const colorsCount = {};
+    for (const person of people) {
+        const { color } = person;
+        colorsCount[color] = colorsCount[color] ? colorsCount[color] + 1 : 1;
+    }
+// randame dazniausiai paminetos spalvos kieki (spalvos pavadinimas nera svarbus)
+    let mostPopularcolorCount = 0;
+    for (const color in colorsCount) {
+        const count = colorsCount[color];
+        if (count > mostPopularcolorCount) {
+            mostPopularcolorCount = count;
+        }
+    }
+// randame visus spalvu pavadinimus, kurie atitinka didziausius skaicius
+    let mostPopularColorNames = [];
+    for (const color in colorsCount) {
+        const count = colorsCount[color];
+        if (count === mostPopularcolorCount) {
+            mostPopularColorNames.push(color);
+        }
+    }
+    console.log(mostPopularColorNames);
 
-// for (let i = 0; i < people.length; i++) {
-//     const person = people[i];
-//     const { color } = person;
-//     colors.push(color);
-// }
 
-for (const person of people) {
-    const color = person.color;
-    colors.push(color);
-}
-console.log(colors);
+
 
