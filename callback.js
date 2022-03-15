48d224d6762a213d443903151f0908e5f3ba7a24
Rozietka <morkvenas.vitalijus@gmail.com>
2022-03-15 21:18:35 +0200

    math-call

---
+++ callback.js
@@ -0,0 +1,20 @@
+console.log('kkkkk');
+
+function spausdinti (text) {
+    return `Stai tavo tekstas: ${text}.`;
+}
+
+function kabutese (text) {
+    return `Tekstas: "${text}".`;
+}
+
+function spausdintuvas (text, format) {
+    return format(text);  // sprendimas
+}
+
+const word = 'Labas';
+const t1 = spausdintuvas(word, spausdinti);
+const t2 = spausdintuvas(word, kabutese);
+
+console.log(t1);    // stai tavo tekstas: Labas.
+console.log(t2);    // tekstas: "Labas"
